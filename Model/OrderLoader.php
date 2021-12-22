507f66f1d87210b9f322f2ce843c386b6747140c
binayak <69388246+itsme17@users.noreply.github.com>
2021-12-22 12:52:09 +0100

    added orderloader: added methods getorders and getordersbyproductid

--- Model/OrderLoader.php
+++ Model/OrderLoader.php
@@ -6,12 +6,23 @@ class OrderLoader extends DataLoader{
 
     public function getOrders(): array {
         $orders = [];
-        $sql = "SELECT * FROM order";
+        $sql = "SELECT * FROM `order` INNER JOIN product ON 'order.id' = product.id;";
         $stmt = $this->connect()->query($sql);
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             array_push($students, $row);
         }
         return $students;
+    }
+
+    public function getOrdersByProductId(int $id) {
+        $orderByProductId = [];
+        $sql = "SELECT name FROM `order` INNER JOIN product ON 'order.id'=product.id WHERE product.id = ?";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$id]);
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            array_push($orderByProductId, $row);
+        }
+        return $orderByProductId;
     }
 }
 

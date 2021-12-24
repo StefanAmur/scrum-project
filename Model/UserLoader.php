1b7457ceab873ec0688da322d441c104f619bbb8
Shreejan Joshi <shreejanjoshi97@gmail.com>
2021-12-24 10:42:13 +0100

    made a add, delete and edit query

--- Model/UserLoader.php
+++ Model/UserLoader.php
@@ -16,5 +16,21 @@ class UserLoader extends DataLoader
         return $users;
     }
 
+    public function addUser(string $firstName, string $lastName, string $userName, string $email, string $password, string $addressLine, string $city, string $postCode, string $country) : void {
+        $sql = "INSERT INTO user (first_name, last_name, username, password, address_line, city, post_code, country) VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$firstName, $lastName, $userName, $email, $password, $addressLine, $city, $postCode, $country]);    
+    }
 
+    public function editUser(string $firstName, string $lastName, string $userName, string $email, string $password, string $addressLine, string $city, string $postCode, string $country, int $id) : void {
+        $sql = "UPDATE user SET first_name=?, last_name=?, username=?, password=?, address_line=?, city=?, post_code=?, country=? WHERE id=?";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$firstName, $lastName, $userName, $email, $password, $addressLine, $city, $postCode, $country, $id]); 
+    }
+
+    public function deleteUser(int $id) : void {
+        $sql = "DELETE FROM user WHERE id = ?";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$id]);
+    }
 }
